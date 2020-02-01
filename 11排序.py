515efb2091538fb370984b82d1bc8d36dbf44715
mangk <mangk.l@qq.com>
2020-02-01 13:44:59 +0800

    冒泡排序和插入排序

---
+++ 11排序.py
@@ -0,0 +1,32 @@
+list = [1, 5, 3, 4, 2]
+
+
+# 冒泡排序
+def bubbleSort(list):
+    count = len(list)
+    for i in range(count):
+        flag = False
+        for j in range(0, count - i - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+                flag = True
+        if not flag: break
+
+
+# 插入排序
+def insertionSort(list):
+    count = len(list)
+    # id1 = id(list)
+    for i in range(count):
+        list, val = list[0:count - 1], list[count - 1: count]  # 注意list还是不是原来的list（id是否已经发生了改变）
+        # id2 = id(list)
+        for j in range(1, count):
+            if val[0] < list[j - 1]:
+                leftList, rightList = list[0: j - 1], list[j - 1: count]
+                list = leftList + val + rightList
+                break
+            if j == count - 1:
+                list = list + val  # 注意list还是不是原来的list（id是否已经发生了改变）
+                # id3 = id(list)
+    # id4 = id(list)
+    return list
